28e43a0b170568d996c630958b8e4f1347aa437c
Grzegorz Sledz <sledzgrzegorz@gmail.com>
2016-08-29 15:42:47 +0200

    Log4j2 was configured. Configuration was adjusted to the specyfic modules

--- parser/src/main/java/main/ParserMain.java
+++ parser/src/main/java/main/ParserMain.java
@@ -1,5 +1,7 @@
 package main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import parser.DTO.Book;
 import parser.PageLoader;
 import parser.PromotionLibrary;
