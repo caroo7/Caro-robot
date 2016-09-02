62ca6947396380242c6a28a2f92123fe8f6772d3
Grzegorz Sledz <sledzgrzegorz@gmail.com>
2016-09-02 10:23:24 +0200

    Fixed empik parser genre problem

--- parser/src/test/java/parser/empik/EmpikBookParserTest.java
+++ parser/src/test/java/parser/empik/EmpikBookParserTest.java
@@ -6,7 +6,6 @@ import org.jsoup.nodes.Document;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import parser.IBookParser;
-import parser.publio.PublioUrlCrawler;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -40,7 +39,7 @@ public class EmpikBookParserTest {
         sf.assertEquals(book.getAuthors(), expectedAuthors);
         sf.assertEquals(book.getPrice(), "19,19 zł");
         sf.assertEquals(book.getPercentageDiscount(), "40%");
-        sf.assertEquals(book.getGenre(), "Kryminał");
+        sf.assertEquals(book.getGenre(), "Literatura");
         sf.assertEquals(book.getDescription(), "Nagroda Wielkiego Kalibru dla najlepszej polskiej powieści kryminalnej i sensacyjnej roku 2007 Teodora Szackiego, warszawskiego prokuratora, łatwo rozpoznać na miejscu zbrodni. Wysoki, szczupły, w zbyt dobrym jak na urzędnika garniturze, o młodej twarzy, z którą kontrastują zupełnie siwe włosy. Stoi trochę z boku, wściekły, że znów kogoś zamordowano po siedemnastej, dokonując zamachu na jego poukładane życie rodzinne lub – zależnie od dnia – przeszkadzając mu w nieudolnym flircie, który może to poukładane życie doszczętnie zburzyć. W chłodną niedzielę 5 czerwca 2005 roku Szacki rozpoczyna nowe śledztwo. W klasztorze,w centrum miasta, zamordowano jednego z uczestników niekonwencjonalnej terapii grupowej, w czasie której pacjenci wcielali się w role swoich bliskich. Przypadkowe zabójstwo podczas włamania? Taka jest oficjalna wersja, gdyż Szackiemu trudno uwierzyć w hipotezę, że sprawcą zbrodni jest któryś z uczestników terapii. A jeśli tak, to dlaczego? Czy motywu należy szukać w nich samych, czy w osobach, które odgrywali podczas terapii? Każde przesłuchanie dostarcza informacji jeszcze bardziej wikłających sprawę. Prokurator ma nadzieję, że dokładne zbadanie przeszłości denata – osoby pozornie nieciekawej i bezbarwnej – pozwoli wykryć przyczynę zabójstwa i znaleźć sprawcę. Są jednak tajemnice, których nie odkrywa się bezkarnie – rodzinne tajemnice strzeżone przez siły potężniejsze niż rodzina. Jerzy Pilch o \"Uwikłaniu\" „Ukazało się Uwikłanie – doskonały polski kryminał współczesny. Tak jest: w literaturze polskiej pojawił się nie lada majster: Zygmunt Miloszewski – pisarz rasowy.” „Miloszewski potrafi dać napięcie nawet w scenie przeglądania starych gazet” „pokazała się bardzo dobra, polska, współczesna powieść kryminalna”");
         sf.assertEquals(book.getUrl(), bookUrl);
         sf.assertEquals(book.getCoverUrl(), coverUrl);
