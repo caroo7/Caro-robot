3d9630979c9d52025086ad1187749afd498103c7
Grzegorz Sledz <sledzgrzegorz@gmail.com>
2016-09-06 11:00:09 +0200

    Incorrect logging was removed form interface IPageUrlParser

--- parser/src/main/java/parser/IPageUrlParser.java
+++ parser/src/main/java/parser/IPageUrlParser.java
@@ -12,15 +12,11 @@ import java.util.stream.Collectors;
 
 public interface IPageUrlParser {
 
-    Logger log = LogManager.getRootLogger();
-
     String getLinkToNextPage(Optional<Document> document);
     List<String> getLinksToBooksDetails(Optional<Document> document);
 
     default List<String> getLinksList(Optional<Document> document, String cssQuery) {
         if(document.isPresent()) {
-            log.debug("cssQuery: '" + cssQuery + "' document: " + document.get().baseUri());
-
             Elements element = document.get().select(cssQuery);
             return element.stream().map(e -> e.absUrl("href")).filter(s -> !s.contains("#")).distinct().collect(Collectors.toList());
         }
