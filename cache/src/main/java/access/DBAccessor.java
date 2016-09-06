db4a83e696f5c1287d999f77520399a952663fc6
YogAzathoth <bartlomiej.lazarski@gmail.com>
2016-09-06 15:46:00 +0200

    Revert "Corrected the bugs"
    This reverts commit b8bf9c376e2bb2f4b01fdf27642986f6ab4df7f5.
    
    Made a mistake in one file, and by mistake pushed the commit to master

--- cache/src/main/java/access/DBAccessor.java
+++ cache/src/main/java/access/DBAccessor.java
@@ -5,24 +5,27 @@ import entities.Library;
 import lombok.extern.log4j.Log4j2;
 import repositories.BookRepository;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
 @Log4j2
-public class DBAccessor implements Serializable {
-
-    private static final String CACHE_FILE_SAVE_LOCATION = "../../web/";
+public class DBAccessor {
 
     public List<Book> getBooks(BookRepository bookRepo, Library library) {
         return bookRepo.findByLibrary(library);
     }
+
+    private static final String CACHE_FILE_SAVE_LOCATION = "../../web/";
+
 
     public void createCache(List<Book> list, String libraryName) {
         try {
             FileOutputStream outputStream = new FileOutputStream(CACHE_FILE_SAVE_LOCATION + libraryName);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
             objectOutputStream.writeObject(list);
-            outputStream.close();
         } catch (FileNotFoundException fNFE) {
             log.error("Exception while accessing the file: " + fNFE);
         } catch (IOException iOE) {
